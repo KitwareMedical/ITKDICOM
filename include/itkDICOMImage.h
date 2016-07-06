e1d15368f7a1a4a60588e84299736ad83785cabf
Zach Mullen <zach.mullen@kitware.com>
2016-07-06 13:18:24 -0400

    Fix docstring

--- include/itkDICOMImage.h
+++ include/itkDICOMImage.h
@@ -23,36 +23,10 @@
 namespace itk
 {
 /** \class DICOMImage
- *  \brief Templated n-dimensional image class.
+ *  \brief DICOM image container.
  *
- * Images are templated over a pixel type (modeling the dependent
- * variables), and a dimension (number of independent variables).  The
- * container for the pixel data is the ImportImageContainer.
- *
- * Within the pixel container, images are modelled as arrays, defined by a
- * start index and a size.
- *
- * The superclass of Image, ImageBase, defines the geometry of the
- * image in terms of where the image sits in physical space, how the
- * image is oriented in physical space, the size of a pixel, and the
- * extent of the image itself.  ImageBase provides the methods to
- * convert between the index and physical space coordinate frames.
- *
- * Pixels can be accessed direcly using the SetPixel() and GetPixel()
- * methods or can be accessed via iterators that define the region of
- * the image they traverse.
- *
- * The pixel type may be one of the native types; a Insight-defined
- * class type such as Vector; or a user-defined type. Note that
- * depending on the type of pixel that you use, the process objects
- * (i.e., those filters processing data objects) may not operate on
- * the image and/or pixel type. This becomes apparent at compile-time
- * because operator overloading (for the pixel type) is not supported.
- *
- * The data in an image is arranged in a 1D array as [][][][slice][row][col]
- * with the column index varying most rapidly.  The Index type reverses
- * the order so that with Index[0] = col, Index[1] = row, Index[2] = slice,
- * ...
+ * In addition to all the functionality of the base Image class, this also
+ * contains DICOM header information.
  *
  * \sa ImageBase
  * \sa ImageContainerInterface
