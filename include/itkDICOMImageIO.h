7e902be0384166f5a885210fa61259bbef21553c
Zach Mullen <zach.mullen@kitware.com>
2016-07-20 14:56:19 -0400

    ImageIO class using DCMTKIO's file reader

---
+++ include/itkDICOMImageIO.h
@@ -0,0 +1,69 @@
+#ifndef itkDICOMImageIO_h
+#define itkDICOMImageIO_h
+
+#include <fstream>
+#include <stdio.h>
+#include "itkImageIOBase.h"
+
+class DicomImage;
+
+namespace itk
+{
+/** \class DICOMImageIO
+ *
+ *  \brief Read DICOM image file format.
+ */
+class DICOMImageIO : public ImageIOBase
+{
+public:
+  typedef DICOMImageIO              Self;
+  typedef ImageIOBase               Superclass;
+  typedef SmartPointer< Self >      Pointer;
+
+  itkNewMacro(Self);
+  itkTypeMacro(DICOMImageIO, ImageIOBase);
+
+  virtual bool CanReadFile(const char*) ITK_OVERRIDE;
+
+  virtual void ReadImageInformation() ITK_OVERRIDE;
+
+  virtual void Read(void*) ITK_OVERRIDE;
+
+  /*-------- This part of the interfaces deals with writing data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can write the
+   * file specified. */
+  virtual bool CanWriteFile(const char *) ITK_OVERRIDE;
+
+  virtual void WriteImageInformation() ITK_OVERRIDE;
+
+  virtual void Write(const void*) ITK_OVERRIDE;
+
+  DICOMImageIO();
+  ~DICOMImageIO();
+
+private:
+  ITK_DISALLOW_COPY_AND_ASSIGN(DICOMImageIO);
+
+  void OpenDicomImage();
+
+  /*----- internal helpers --------------------------------------------*/
+  bool m_UseJPEGCodec;
+  bool m_UseJPLSCodec;
+  bool m_UseRLECodec;
+
+  DicomImage* m_DImage;
+
+  bool m_DicomImageSetByUser;
+
+  double      m_RescaleSlope;
+  double      m_RescaleIntercept;
+  std::string m_LastFileName;
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkDICOMImageIO.hxx"
+#endif
+
+#endif // itkDICOMImageIO_h
--- itk-module.cmake
+++ itk-module.cmake
@@ -3,7 +3,10 @@ set(DOCUMENTATION "Adds support for DICOM IO and anonymization.")
 itk_module(DICOM
   DEPENDS
     ITKCommon
+    ITKIOImageBase
+    ITKDCMTK
     ITKIODCMTK
+    ITKZLIB
   TEST_DEPENDS
     ITKTestKernel
   EXCLUDE_FROM_DEFAULT
