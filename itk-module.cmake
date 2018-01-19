f9e081623a20cf19ab263d42837f670065c12017
Matt McCormick <matt.mccormick@kitware.com>
2018-01-18 23:19:27 -0500

    ENH: Use CircleCI 2.0 configuration

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,4 +1,7 @@
-set(DOCUMENTATION "Adds support for DICOM IO and anonymization.")
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
 
 itk_module(DICOM
   DEPENDS
@@ -6,7 +9,8 @@ itk_module(DICOM
     ITKIODCMTK
   TEST_DEPENDS
     ITKTestKernel
-  EXCLUDE_FROM_DEFAULT
   DESCRIPTION
     "${DOCUMENTATION}"
+  EXCLUDE_FROM_DEFAULT
+  ENABLE_SHARED
 )
--- test/Docker/test.sh
+++ test/Docker/test.sh
@@ -6,16 +6,29 @@
 set -x
 set -o
 
-cd /usr/src/ITKDICOM
+cd /ITKDICOM
 branch=$(git rev-parse --abbrev-ref HEAD)
 date=$(date +%F_%H_%M_%S)
 sha=$(git rev-parse --short HEAD)
 
-cd /usr/src/ITKDICOM-build
-cmake \
-  -G Ninja \
-  -DITK_DIR:PATH=/usr/src/ITK-build \
-  -DCMAKE_BUILD_TYPE:STRING=Release \
-  -DBUILDNAME:STRING=External-DICOM-${branch}-${date}-${sha} \
-    /usr/src/ITKDICOM
-ctest -VV -D Experimental -E KWStyle
+cd /ITKDICOM-build
+curl -L https://raw.githubusercontent.com/InsightSoftwareConsortium/ITK/dashboard/itk_common.cmake -O
+SHASNIP=$(echo $sha | cut -c1-7)
+cat > dashboard.cmake << EOF
+set(CTEST_SITE "Docker")
+set(CTEST_BUILD_NAME "External-ITKDICOM-${SHASNIP}")
+set(CTEST_BUILD_CONFIGURATION "MinSizeRel")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_FLAGS: "-j5")
+set(CTEST_SOURCE_DIRECTORY /ITKDICOM)
+set(CTEST_BINARY_DIRECTORY /ITKDICOM-build)
+set(dashboard_model Experimental)
+set(dashboard_no_clean 1)
+set(dashboard_no_update 1)
+set(dashboard_cache "
+ITK_DIR:PATH=/ITK-build
+BUILD_TESTING:BOOL=ON
+")
+include(\${CTEST_SCRIPT_DIRECTORY}/itk_common.cmake)
+EOF
+ctest -j 2 -E KWStyle -VV -S dashboard.cmake
